9268c012ef01359b722088c45a2d77e7ac43e94e
gaoxun <gaoxun@zhihu.com>
2017-12-23 12:52:39 +0800

    Add logger in stats package

---
+++ common/stats.go
@@ -0,0 +1,14 @@
+package common
+
+import "github.com/op/go-logging"
+
+func createLogger() *logging.Logger {
+	//set logger format globally
+	var format = logging.MustStringFormatter(
+		`[%{level:.4s}] [%{shortpkg} %{shortfile} %{shortfunc}] %{message}`,
+	)
+	logging.SetFormatter(format)
+	return logging.MustGetLogger("")
+}
+
+var Logger = createLogger()
